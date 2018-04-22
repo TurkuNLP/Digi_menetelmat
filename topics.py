eb1f77ade667d3bab3d6791ff142414fb37b9e32
mavela <mavela@utu.fi>
2018-04-22 15:18:14 +0000

    ready

--- topics.py
+++ topics.py
@@ -3,19 +3,10 @@ import sys
 
 solr=pysolr.Solr("http://localhost:23456/solr/s24_top")
 
-result2=solr.search(q="*.*", sort="date_dt desc", fl="id,best1_s")
-result = solr.search(q="thread_txt_fi:koira")
-result3 = solr.search(q="best1_s:lemmikki", sort="best1_f desc", fl="id,thread_txt_fi")
+result=solr.search(q="*.*", fl="best1_s")
+#result2=solr.serach(q="thread_txt_fi:Turku", fl="best1_s")
 
-print("Found this many:", result3.hits)
-print("Here they are:", result3.docs)
-
+print("Found this many:", result.hits)
+print("Here they are:", result.docs)
 
 
-
-labels = open("topicnames.txt", 'r')
-
-#for label in labels:
-#    label=label.strip()
-#    response=solr.search("best1_s:t_{}".format(label))
-#    print("topic", label, "hits", response.hits)
